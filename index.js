9ac7209709ee66202e748871331ba28535fe9f0d
Derek Jensen <derek.jensen5@gmail.com>
2016-03-24 10:45:15 +0000

    Initial Commit, only one random image
    Chooses a single image from the array and sets it as the background.

---
+++ index.js
@@ -0,0 +1,42 @@
+
+var Cheerio = require('cheerio');
+var request = require('request');
+var http = require('http');
+var Wallpaper = require('wallpaper');
+var fs = require('fs');
+
+var search = "nature";
+var resolution = "1920x1080";
+
+var url = "http://alpha.wallhaven.cc/search?q="+search+
+	"&categories=100&purity=100&resolutions="+resolution+
+	"&sorting=random&order=desc";
+	
+var imgurl = "http://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-";
+
+function doIt() {
+
+	request(url, function(error, response, html) {
+		if(!error && response.statusCode == 200) {
+			var $ = Cheerio.load(html);
+			var wallpaperIds = [];
+			$('figure.thumb').each(function(figure){
+				figure = $(this);
+				wallpaperIds.push(figure.attr('data-wallpaper-id'));
+			});
+
+			var id = wallpaperIds[Math.floor(Math.random() * wallpaperIds.length)];
+
+			var file = fs.createWriteStream("wallhaven-"+id+".jpg");
+			var req = http.get(imgurl + id + ".jpg", function(res) {
+					res.pipe(file);
+			});
+
+			Wallpaper.set("wallhaven-"+id+".jpg").then(function() {
+				console.log("Wallpaper Set");
+			});
+		}
+	});
+}
+
+doIt();
