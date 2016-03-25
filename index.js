fc53c57c6b8694da86eeaa4385f4540a8a36677e
Derek Jensen <derek.jensen5@gmail.com>
2016-03-25 22:54:44 +0000

    Accepting Resolution and Search args
    User can now pass in a resolution and search argument so you don't need to modify the file.

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ var fs = require('fs');
 var search = "nature";
 var resolution = "1920x1080";
 
-var url = "http://alpha.wallhaven.cc/search?q="+search+
-	"&categories=100&purity=100&resolutions="+resolution+
-	"&sorting=random&order=desc";
-
 var imgurl = "http://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-";
 
-function doIt() {
+function setWallpaper(resolution, search) {
+
+	var url = "http://alpha.wallhaven.cc/search?q="+search+
+		"&categories=100&purity=100&resolutions="+resolution+
+		"&sorting=random&order=desc";
 
 	request(url, function(error, response, html) {
 		if(!error && response.statusCode == 200) {
@@ -46,4 +46,19 @@ function doIt() {
 	});
 }
 
-doIt();
+var args = process.argv.slice(2);
+
+if(args.length !== 2)
+{
+	console.log("Incorrect Parameters! Defaulting to "+resolution+
+	" and searching "+search+"\n\nUsage:\n\tnode index.js <resolution> <search-query>");
+	console.log("\tnpm start <resolution> <search-query>");
+	console.log("\nExample:");
+	console.log("\tnode index.js 1920x1080 space\n");
+}
+else {
+	resolution = args[0];
+	search = args[1];
+}
+
+setWallpaper(resolution, search);
